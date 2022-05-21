47589df270d79f74820ed456e81776e51d3cc6bb
Marcin Malinowski <mmalinowski@evolution.com>
2022-05-21 18:30:02 +0200

    DayOfTime unit tests update

--- src/lib/dpts/formats/time-of-day.spec.ts
+++ src/lib/dpts/formats/time-of-day.spec.ts
@@ -32,31 +32,31 @@ describe("TimeOfDay", () => {
     })
     describe("decode", () => {
         it("time", () => {
-            expect(fromBuffer(Buffer.from([0x00, 0x2f, 0x2a, 0x07]))).toEqual("Mon 15:42:07")
+            expect(fromBuffer(Buffer.from([0x00, 0x2f, 0x2a, 0x07]))).toEqual([1, 15, 42, 7])
         })
         
         it("time", () => {
-            expect(fromBuffer(Buffer.from([0x00, 0x57, 0x0b, 0x22]))).toEqual("Tue 23:11:34")
+            expect(fromBuffer(Buffer.from([0x00, 0x57, 0x0b, 0x22]))).toEqual([2, 23, 11, 34])
         })
         
         it("time", () => {
-            expect(fromBuffer(Buffer.from([0x00, 0x6a, 0x29, 0x3b]))).toEqual("Wed 10:41:59")
+            expect(fromBuffer(Buffer.from([0x00, 0x6a, 0x29, 0x3b]))).toEqual([3, 10, 41, 59])
         })
         
         it("time", () => {
-            expect(fromBuffer(Buffer.from([0x00, 0xa9, 0x2c, 0x0c]))).toEqual("Fri 09:44:12")
+            expect(fromBuffer(Buffer.from([0x00, 0xa9, 0x2c, 0x0c]))).toEqual([5, 9, 44, 12])
         })
 
         it("no day", () => {
-            expect(fromBuffer(Buffer.from([0x00, 0x0b, 0x1e, 0x00]))).toEqual("11:30:00")
+            expect(fromBuffer(Buffer.from([0x00, 0x0b, 0x1e, 0x00]))).toEqual([0, 11, 30, 0])
         })
 
         it("no seconds", () => {
-            expect(fromBuffer(Buffer.from([0x00, 12, 15, 0]))).toEqual("12:15:00")
+            expect(fromBuffer(Buffer.from([0x00, 12, 15, 0]))).toEqual([0, 12, 15, 0])
         })
 
         it("no zeros", () => {
-            expect(fromBuffer(Buffer.from([0x00, 7, 12, 0]))).toEqual("07:12:00")
+            expect(fromBuffer(Buffer.from([0x00, 7, 12, 0]))).toEqual([0, 7, 12, 0])
         })
     })
 })
--- src/lib/dpts/formats/time-of-day.ts
+++ src/lib/dpts/formats/time-of-day.ts
@@ -2,30 +2,30 @@ import { DataPointAbstract } from "./data-point-abstract"
 
 enum DayOfWeek {
     "" = 0,
-    Mon = 0x20,
-    Tue = 0x40,
-    Wed = 0x60,
-    Thu = 0x80,
-    Fri = 0xa0,
-    Sat = 0xc0,
-    Sun = 0xe0
+    Mon = 1,
+    Tue = 2,
+    Wed = 3,
+    Thu = 4,
+    Fri = 5,
+    Sat = 6,
+    Sun = 7
 }
 
 const pattern = /^(?:(Mon|Tue|Wed|Thu|Fri|Sat|Sun) )?(2[0-4]|[01]?[0-9]):([0-5]?[0-9])(?::([0-5]?[0-9]))?$/
-export function fromBuffer(data: Buffer): string {
-    const d = DayOfWeek[data.readUint8(1) & 0xe0]
+export function fromBuffer(data: Buffer): number[] {
+    const d = (data.readUint8(1) & 0xe0) >> 5
     const h = data.readUint8(1) & 0x1f
     const m = data.readUint8(2)
     const s = data.readUint8(3)
     
-    return `${d ? d + " " : ""}${h.toString(10).padStart(2, "0")}:${m.toString(10).padStart(2, "0")}:${s.toString(10).padStart(2, "0")}`
+    return [d, h, m, s]
 }
 
 export function toBuffer(value: string, data: Buffer): Buffer {
     if (pattern.test(value)) {
         const [, d, h, m, s] = value.match(pattern)
 
-        data.writeUint8(DayOfWeek[d || ""] + +h, 1)
+        data.writeUint8((DayOfWeek[d || ""] << 5) + +h, 1)
         data.writeUint8(+m, 2)
         data.writeUint8(+s || 0, 3)
 
@@ -39,7 +39,9 @@ export function toBuffer(value: string, data: Buffer): Buffer {
 export abstract class TimeOfDay extends DataPointAbstract<string> {
 
     protected decode(data: Buffer): string {
-        return fromBuffer(data)
+        const [d, h, m, s] = fromBuffer(data)
+
+        return `${d ? DayOfWeek[d] + " " : ""}${h.toString(10).padStart(2, "0")}:${m.toString(10).padStart(2, "0")}:${s.toString(10).padStart(2, "0")}`
     }
 
     public async write(value: string): Promise<void> {
