b1f59efa96272e8eb7f320ce728da8be94450f8f
Marcin Malinowski <mmalinowski@evolution.com>
2022-05-21 13:42:28 +0200

    Float16 read/write unit tests

---
+++ src/lib/dpts/formats/f16.spec.ts
@@ -0,0 +1,33 @@
+import { toBuffer, fromBuffer } from "./f16"
+
+describe('Float16', () => {
+    describe('fromBuffer', () => {
+        
+        it('should read float number', () => {
+            expect(fromBuffer(Buffer.from([0, 0x0c, 0x68]))).toEqual(22.56)
+        })    
+        
+        it('should read float number', () => {
+            expect(fromBuffer(Buffer.from([0, 0x00, 0x00]))).toEqual(0)
+        })    
+        
+        it('should read float number', () => {
+            expect(fromBuffer(Buffer.from([0, 0xff, 0xff]))).toEqual(-670760.96)
+        })    
+    })
+
+    describe('fromBuffer', () => {
+        
+        it('should write float number', () => {
+            expect(toBuffer(22.56, Buffer.alloc(3))).toEqual(Buffer.from([0, 0x0c, 0x68]))
+        })    
+        
+        it('should write float number', () => {
+            expect(toBuffer(0, Buffer.alloc(3))).toEqual(Buffer.from([0, 0x00, 0x00]))
+        })    
+        
+        it('should write float number', () => {
+            expect(toBuffer(-670760.96, Buffer.alloc(3))).toEqual(Buffer.from([0, 0xff, 0xff]))
+        })    
+    })
+})
--- src/lib/dpts/formats/f16.ts
+++ src/lib/dpts/formats/f16.ts
@@ -1,27 +1,40 @@
 import { DataPointAbstract } from "./data-point-abstract"
 
+export function fromBuffer(buf: Buffer): number {
+    const value = buf.readUint16BE(1)
+    const e = (value >> 11) & 0x0f
+    const m = value & 0x07ff
+    const s = value & 0x8000
+
+    return 0.01 * m * 2**e * (s ? -1 : 1)
+}
+
+export function toBuffer(value: number, buf: Buffer): Buffer {
+    let m = Math.abs(value * 100)
+    let s = value < 0 ? 1 : 0
+    let e = 0
+
+    while(m > 0x07ff) {
+        m /= 2
+        e++
+    }
+
+    if (e > 15) {
+        throw new Error('Float16: Value Out of Range: ' +value)
+    }
+
+    buf.writeUint16BE((s << 15) + (e << 11) + m, 1)
+    buf.writeUint8(0, 0)
+    return buf
+}
+
 export abstract class F16 extends DataPointAbstract<number> {
-    private fromBuffer(buf: Buffer): number {
-        const value = buf.readUint16BE(1)
-        const e = (value >> 11) & 0x0f
-        const m = value & 0x07ff
-        const s = value & 0x8000
-
-        return 0.01 * m * 2**e * (s ? -1 : 1)
-    }
-
-    private toBuffer(value: number, buf: Buffer): Buffer {
-        buf.writeUint16BE(0xffff, 1)
-        buf.writeUint8(0x80, 0)
-        return buf
-    }
-
     protected decode(data: Buffer): number {
-        return this.fromBuffer(data)
+        return fromBuffer(data)
     }
 
     public async write(value: number): Promise<void> {
-        return this.send(this.toBuffer(value, Buffer.alloc(3)))
+        return this.send(toBuffer(value, Buffer.alloc(3)))
     }
 
     public removeValueListener(cb: (value: number, unit: string, source: string) => void) {
