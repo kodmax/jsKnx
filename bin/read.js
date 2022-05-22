525eb5e49d80d03f2cc7f04ba2bd641d66b24fd6
Marcin Malinowski <mmalinowski@evolution.com>
2022-05-22 20:35:09 +0200

    npm publish

---
+++ bin/read.js
@@ -0,0 +1,25 @@
+#!/usr/local/bin/node
+const { KnxLink } = require("js-knx")
+const dpts = require("js-knx")
+
+const [ address, dpt ] = process.argv.slice(2)
+if (!dpts ["DPT_" + dpt]) {
+    console.error("Unknown DPT: " + dpt)
+    process.exit(2)
+
+} else {
+    KnxLink.connect("192.168.0.8").then(async knx => {
+        knx.group(address, dpts ["DPT_" + dpt], dp => {
+            dp.addValueListener((value, unit) => {
+                console.log(value, unit)
+                knx.disconnect().then(() => process.exit(0))
+            })
+            dp.requestValue()
+        })
+        
+        process.on("SIGINT", () => {
+            knx.disconnect().then(() => process.exit(0))
+        })
+    })    
+}
+
---
+++ bin/write.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+const { KnxLink } = require("js-knx")
+const dpts = require("js-knx")
+
+const [ address, dpt, value ] = process.argv.slice(2)
+if (!dpts ["DPT_" + dpt]) {
+    console.error("Unknown DPT: " + dpt)
+    process.exit(2)
+
+} else {
+    KnxLink.connect("192.168.0.8").then(async knx => {
+        await knx.group(address, dpts ["DPT_" + dpt]).write(value).then(() => knx.disconnect()).then(() => process.exit(0))
+    })    
+}
