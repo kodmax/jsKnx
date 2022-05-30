5db91aa7db3c9a96051ccacc97b583e1ba550fee
Marcin Malinowski <mmalinowski@evolution.com>
2022-05-30 18:28:38 +0200

    update toString for some dpts

--- src/lib/dpts/f32.ts
+++ src/lib/dpts/f32.ts
@@ -9,6 +9,15 @@ export class DPT_Value_Power extends F32 {
 export class DPT_Value_Frequency extends F32 {
     public readonly type: DPT = DPT.Value_Frequency
     public readonly unit: string = "Hz"
+
+    public toString(value?: number): string {
+        if (value === undefined) {
+            return `${this.address} (${this.type})`
+
+        } else {
+            return `${Number(value).toFixed(2)} ${this.unit}`
+        }
+    }
 }
 
 export class DPT_Value_Electric_Potential extends F32 {
@@ -19,14 +28,32 @@ export class DPT_Value_Electric_Potential extends F32 {
 export class DPT_Value_Electric_Current extends F32 {
     public readonly type: DPT = DPT.Value_Electric_Current
     public readonly unit: string = "A"
+
+    public toString(value?: number): string {
+        if (value === undefined) {
+            return `${this.address} (${this.type})`
+
+        } else {
+            return `${Number(value).toFixed(3)} ${this.unit}`
+        }
+    }
 }
 
 export class DPT_Value_Power_Factor extends F32 {
     public readonly type: DPT = DPT.Value_Power_Factor
-    public readonly unit: string = "°"
+    public readonly unit: string = ""
+
+    public toString(value?: number): string {
+        if (value === undefined) {
+            return `${this.address} (${this.type})`
+
+        } else {
+            return `${Number(value).toFixed(2)}`
+        }
+    }
 }
 
-export class Value_ApparentPower extends F32 {
+export class DPT_Value_ApparentPower extends F32 {
     public readonly type: DPT = DPT.Value_ApparentPower
-    public readonly unit: string = "W"
+    public readonly unit: string = "VA"
 }
