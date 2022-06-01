731cb9868f63cd9009b33552d4636a7c18c71eb7
Marcin Malinowski <mmalinowski@evolution.com>
2022-06-01 07:32:04 +0200

    temp fixed(0)

--- src/lib/dpts/f16.ts
+++ src/lib/dpts/f16.ts
@@ -4,15 +4,6 @@ import { DPT } from "../enums"
 export class DPT_Value_Temp extends F16 {
     public readonly type: DPT = DPT.Value_Temp
     public readonly unit: string = "°C"
-
-    public toString(value?: number): string {
-        if (value === undefined) {
-            return `${this.address} (${this.type})`
-
-        } else {
-            return `${Number(value).toFixed(1)} ${this.unit}`
-        }
-    }
 }
 
 export class DPT_Value_AirQuality extends F16 {
